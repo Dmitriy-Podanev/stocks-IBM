0da9855dab5af186fcaec1b1037556b82412c12b
Isla <podaneovd@mail.ru>
2020-11-03 23:16:09 +0300

    first index

--- index.py
+++ index.py
@@ -2,9 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df_file = pd.read_csv("IBM.csv", sep=";", parse_dates=['<DATE>'], dayfirst=True, index_col="<DATE>")
+vol=[]
+cls=[]
+for i in df_file.index:
+   vol.append(df_file.loc[f"{i}", "<VOL>"])
+   print(df_file.loc[f"{i}", "<VOL>"])
 
-new_sample = df_file.loc["2012":"2020", "<CLOSE>"]
-plt.plot(new_sample)
+plt.plot(df_file.index,vol)
 
 plt.grid()
 plt.show()
